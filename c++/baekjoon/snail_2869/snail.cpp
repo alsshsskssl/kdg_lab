db53deec695bd0b93c6442621f7bdc2318238a15
dongkun kang <dongkunkang@dongkunui-MacBookPro.local>
2019-10-04 16:48:41 +0900

    sfsd

---
+++ c++/baekjoon/snail_2869/snail.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int calc(int A,int B,int T);
+int calc();
+int main(){
+	int A,B,T;
+	cin >> A >> B >> T;
+	
+	cout << calc(A,B,T) << endl;
+	
+	return 0;
+}
+int calc_ing(){
+	
+}
+int calc(int A,int B,int T){
+	
+	return 0;
+}
